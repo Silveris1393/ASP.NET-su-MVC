74c00722cbe43bedd747a0c261d0cd0605ec7028
Dell <Dell@DESKTOP-I12KEI5>
2022-04-13 17:08:43 +0300

    Add project files.

---
+++ ASP.NET su MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASP.NET_su_MVC.Models;
+
+namespace ASP.NET_su_MVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+       private static List<SunsViewModel> sunys = new List<SunsViewModel>();
+        public IActionResult Index()
+        {
+           
+            return View(sunys);
+        }
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+        public IActionResult CreateDog(SunsViewModel sunsModelis)
+        {
+            sunys.Add(sunsModelis);
+            //  return View("Index");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {  
+            var sunsVm = new SunsViewModel();
+            return View(sunsVm);
+        }
+    }
+}
---
+++ ASP.NET su MVC/Models/SunsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_su_MVC.Models
+{
+    public class SunsViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
